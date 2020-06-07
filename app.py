0059230680edfa3b16bb44f3288b41de78247737
KristinaU <urosova.k@gmail.com>
2020-06-07 15:14:07 +0100

    First (hello) commit

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 
 @app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
