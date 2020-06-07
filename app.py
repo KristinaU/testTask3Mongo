02aca769268c1340b9b18ddcfd6e46a326e87e65
KristinaU <urosova.k@gmail.com>
2020-06-07 20:06:52 +0100

    Add login basics

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime, timedelta
+import random
 
 import pymongo
 from flask import Flask, request
@@ -44,8 +46,32 @@ def registration():
 # list of all users
 @app.route('/users', methods=['GET'])
 def users():
-    result = 'Here are the users: ' + (str(collection.distinct('username')))
+    result = 'So we got the users as: ' + (str(collection.distinct('username')))
     return result, 200
+
+
+# login functionality
+@app.route('/login', methods=['POST'])
+def login():
+    start_time = datetime.now()
+
+    expire_time = datetime.now() + timedelta(minutes=+30)
+
+    letters = 'abcdefghyjklmnopqrstuvwxyz1234567890'
+
+    if check_user(request.form['username'], request.form['password']):
+        print('Now is ' + str(start_time))
+        print('Token expires at ' + str(expire_time))
+        return ''.join(random.choice(letters) for i in range(32))
+    else:
+        return 400
+
+
+def check_user(username, password):
+    if username is not None and password is not None:
+        return True, 200
+    else:
+        return False, 400
 
 
 if __name__ == '__main__':
